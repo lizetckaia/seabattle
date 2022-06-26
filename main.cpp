56f9f533b48de0ad9ecd268500d83b27a79b4338
mskaynova <91434531+mskaynova@users.noreply.github.com>
2022-06-26 14:20:17 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,225 +1,15 @@
 #include <iostream>
 #include <cmath>
-#include <ctime>
 #include <string>
-#include <fstream>
 #include <windows.h>
-#include <conio.h>
-#include <math.h>
-//#include "stdafx.h"
 
 using namespace std;
 
 const int N = 10;
 const int NUM_SHIPS = 9;
 
-int ships_id = 1;
-int ships[10] = {0};
-
-//функция, которая переставляет курсор в заданные координаты
-void gotoxy(int x, int y)
-{
-    COORD p = {static_cast<SHORT>(x), static_cast<SHORT>(y)};
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), p);
-}
-//функция, показывающая корабль в зависимости от параметров
-//параметры: координата x, координата y, положение корабля, размер корабля
-void ship_show(int x, int y, int dir, int ship_size)
-{
-    for (int i = 0; i < ship_size; i++)
-    {
-        gotoxy(x+1 ,y+1);
-        cout << '#';
-
-        switch (dir)
-        {
-            case 0:
-                x++;
-            break;
-            case 1:
-                y++;
-            break;
-            case 2:
-                x--;
-            break;
-            case 3:
-                y--;
-            break;
-        }
-    }
-}
-//функция проверки нахождения корабля в пределах карты
-bool ship_in_map(int x, int y, int dir, int ship_size)
-{
-    bool in_map = 1;
-    //генерация направления кораблей
-    dir = rand() % 4;
-    //проверка возможности постановки корабля
-    for (int i = 0; i < ship_size; i++)
-    {
-        if (x < 0 || y < 0 || x >= N || y >= N)
-        {
-            in_map = 0;
-            break;
-        }
-
-        switch(dir)
-        {
-            case 0:
-                x++;
-                break;
-            case 1:
-                y++;
-                break;
-            case 2:
-                x--;
-                break;
-            case 3:
-                y--;
-                break;
-        }
-    }
-    return in_map;
-}
-//установился ли корабль
-bool set_ship(int map[N][N], int x, int y, int dir, int ship_size)
-{
-    int temp_x = x;
-    int temp_y = y;
-
-    bool setting_is_possible = 1;
-    //проверка возможности постановки корабля
-    for (int i = 0; i < ship_size; i++)
-    {
-        if (x < 0 || y < 0 || x >= N || y >= N)
-        {
-            setting_is_possible = 0;
-            break;
-        }
-
-//if (map[x  ][y]   >= 1 ||
-//map[x  ][y+1] >= 1 ||
-//map[x  ][y-1] >= 1 ||
-//map[x+1][y]   >= 1 ||
-//map[x+1][y+1] >= 1 ||
-//map[x+1][y-1] >= 1 ||
-//map[x-1][y]   >= 1 ||
-//map[x-1][y+1] >= 1 ||
-//map[x-1][y-1] >= 1)
-//{
-//setting_is_possible = 0;
-//break;
-//}
-
-        if (map[x][y]   >= 1)
-        {
-            setting_is_possible = 0;
-            break;
-        }
-        if (y < N-1)
-            if (map[x  ][y+1] >= 1)
-            {
-                setting_is_possible = 0;
-                break;
-            }
-        if (y > 0)
-            if (map[x  ][y-1] >= 1)
-            {
-                setting_is_possible = 0;
-                break;
-            }
-        if (x < N-1)
-            if (map[x+1][y]   >= 1)
-            {
-                setting_is_possible = 0;
-                break;
-            }
-        if (x < N-1 && y < N-1)
-            if (map[x+1][y+1] >= 1)
-            {
-                setting_is_possible = 0;
-                break;
-            }
-        if (x < N-1 && y > 0)
-            if (map[x+1][y-1] >= 1)
-            {
-                setting_is_possible = 0;
-                break;
-            }
-        if (x >0)
-            if (map[x-1][y]   >= 1)
-            {
-                setting_is_possible = 0;
-                break;
-            }
-        if (x > 0 && y < N-1)
-            if (map[x-1][y+1] >= 1)
-            {
-                setting_is_possible = 0;
-                break;
-            }
-        if (x > 0 && y > 0)
-            if (map[x-1][y-1] >= 1)
-            {
-                setting_is_possible = 0;
-                break;
-            }
-
-
-        switch(dir)
-        {
-            case 0:
-                x++;
-            break;
-            case 1:
-                y++;
-            break;
-            case 2:
-                x--;
-            break;
-            case 3:
-                y--;
-            break;
-        }
-    }
-
-//ставим корабль, если есть такая возможность
-    if (setting_is_possible == 1)
-    {
-        x = temp_x;
-        y = temp_y;
-
-        for (int i = 0; i < ship_size; i++)
-        {
-            map[x][y] = ships_id;
-
-            switch(dir)
-            {
-                case 0:
-                    x++;
-                break;
-                case 1:
-                    y++;
-                break;
-                case 2:
-                    x--;
-                break;
-                case 3:
-                    y--;
-                break;
-            }
-        }
-        ships[ships_id] = ship_size;
-
-        ships_id++;
-    }
-
-    return setting_is_possible;
-}
-
 //функция, ставящая на поле корабли
 //три параметра: поле, размер корабля, идентификатор корабля
-
 void set_ships_rand(int map[N][N],int ship_size, int ship_id)
 {
     int x, y;
@@ -315,9 +105,6 @@ void set_ships_rand(int map[N][N],int ship_size, int ship_id)
                         break;
                 }
             }
-            //ships[ships_id] = ship_size;
-
-            //ships_id++;
             count_ship++;
         }
     }
@@ -325,8 +112,9 @@ void set_ships_rand(int map[N][N],int ship_size, int ship_id)
 
 //функция, выводящая игровое поле на экран
 //параметры: исходный массив и маска
-void map_show(int map[N][N], int mask[N][N])
+void map_show(int map[N][N], int mask[N][N], bool use_mask)
 {
+    //прорисовка
     cout << " ";
 
     for (int i = 0; i < N; i++)
@@ -341,7 +129,7 @@ void map_show(int map[N][N], int mask[N][N])
 
         for (int j = 0; j < N; j++)
         {
-            //if (mask[j][i] == 1)
+            if (mask[j][i] == 1 || use_mask == 0)
             {
                 if (map[j][i] == 0)
                 {
@@ -351,18 +139,52 @@ void map_show(int map[N][N], int mask[N][N])
                 {
                     cout << "X";
                 }
+                else if (map[j][i] == -2)
+                {
+                    cout << ".";
+                }
                 else
                 {
                     cout << map[j][i];
                 }
             }
-            //else
-            //{
-            //cout << ' ';
-            //}
+            else
+            {
+            cout << ' ';
+            }
         }
         cout << endl;
     }
+}
+//функция, которая возвращает целое значение в зависимости от того корабль ранен, убит или был совершен промах
+//параметры: ссылка на карту, координата x, координата y, ссылка на массив, в котором хранится информация о жизнях корабля, ссылка на маску
+int shot(int map[N][N], int x, int y, int ships[NUM_SHIPS+1], int mask[N][N])
+{
+    int result = 0; //переменная, которая отвечает за результат стрельбы
+
+    if (map[x][y] >= 1)
+    {
+        ships[map[x][y]]--;
+
+        if (ships[map[x][y]] <= 0)
+        {
+            result = 2;
+        }
+        else
+        {
+            result = 1;
+        }
+
+        map[x][y] = -1;
+    }
+    else
+    {
+        map[x][y] = -2;
+    }
+
+    mask[x][y] = 1;
+
+    return result;
 }
 
 //основная функция
@@ -378,6 +200,7 @@ int main()
 
 
         int mask [N][N] = {0};
+        int mask2 [N][N] = {0};
 
         //добавляем корабли
         for (int i =1; i < NUM_SHIPS; i++)
@@ -388,129 +211,61 @@ int main()
         {
             set_ships_rand(map2, ships2[i], i);
         }
-        /*set_ships_rand(map, 4, 1); //установить 1 4-х палубный корабль
-        set_ships_rand(map, 3, 2); //установить 2 3-х палубных корабля
-        set_ships_rand(map, 2, 3); //установить 3 2-х палубных корабля
-        set_ships_rand(map, 1, 3); //установить 3 1-х палубных корабля
-
-        ships_id = 1;*/
-
-        /*set_ships_rand(map2, 4, 1); //установить 1 4-х палубный корабль
-        set_ships_rand(map2, 3, 2); //установить 2 3-х палубных корабля
-        set_ships_rand(map2, 2, 3); //установить 3 2-х палубных корабля
-        set_ships_rand(map2, 1, 3); //установить 3 1-х палубных корабля*/
 
         int x = 2, y = 2; //координаты первой палубы корабля
         int dir = 1; //определяет положение корабля
         int ship_size = 4; //размер корабля
         int ch;
 
-        /*while (true)
-        {
-            map_show(map,mask);
-            ship_show(x, y, dir, ship_size);
+        bool turn = 1; //переменная, отвечающая за очередность ходов(1 - ходит человек, 0 - компьютер)
 
-            int temp_x = x;
-            int temp_y = y;
-            int temp_dir = dir;
-
-            ch = _getch();
-
-            //изменить координаты или направление
-            switch (ch)
-            {
-                case 100: //d вправо
-                    x++;
-                break;
-                case 115://s вниз
-                    y++;
-                break;
-                case 97://a влево
-                    x--;
-                break;
-                case 119://w вверх
-                    y--;
-                break;
-                case 114://r поворот
-                    dir = !dir;
-                break;
-                case 13://установка корабля
-                    if (set_ship(map, x, y, dir, ship_size))
-                    {
-                        x = 0;
-                        y = 0;
-                        dir = 0;
-                        ship_size--;
-                    }
-                    break;
-            }
-
-            if (!ship_in_map(x, y, dir, ship_size))
-            {
-                int x = temp_x;
-                int y = temp_y;
-                int dir = temp_dir;
-            }
-
-
-
-            system("cls");
-        }*/
-
+        //цикл, отвечающий за стрельбу
         while (true)
         {
-            map_show(map,mask);
-            map_show(map2,mask);
-
-            cout << endl << "Enter coordinates" << endl;
-
-            cin >> x;
-            cin >> y;
-
-            if (map[x][y] >= 1)
+            int result_shot = 0;
+            //цикл, отвечающий за переключение хода
+            do
             {
-                ships[map[x][y]]--;
+                map_show(map, mask, 0);
+                cout << endl;
+                map_show(map2, mask2, 1);
 
-                if (ships[map[x][y]] <= 0) {
+                if (turn == 1) //предоставление хода человеку
+                {
+                    cout << endl << "Enter coordinates";
+                    cin >> x;
+                    cin >> y;
+                    result_shot = shot(map2, x, y, ships2, mask2);
+                }
+                else //предоставление хода компьютеру
+                {
+                    cout << endl << "Computer's turn";
+                    Sleep(1000); //компьютер думает
+                    x = rand() % N;
+                    y = rand() % N;
+                    result_shot = shot(map, x, y, ships1, mask);
+                }
+
+                if (result_shot == 1)
+                {
+                    cout << "True" << endl;
+                }
+
+                else if (result_shot == 2)
+                {
                     cout << "Killed" << endl;
                 }
                 else
                 {
-                    cout << "True" << endl;
+                    cout << "False" << endl;
                 }
-                map[x][y] = -1;
 
-                mask[x][y] = 1;
-                bool ship_detect = false;
-                for (int i = 0; i < N; i++)
-                {
-                    for (int j = 0; j < N; j++)
-                    {
-                        if (map[i][j] == 1)
-                        {
-                            ship_detect = true;
-                            break;
-                        }
-                    }
-                    if (ship_detect)
-                    {
-                        break;
-                    }
-                }
-                if (!ship_detect)
-                {
-                    cout << "You won!" << endl;
-                    break;
-                }
-            }
-            else
-            {
-                cout << "False" << endl;
-            }
-            mask[x][y] = 1;
+                Sleep(1000);
+                system("cls");
+
+            } while (result_shot != 0);
+            turn = !turn;//передача хода
         }
-        break;
-
     }
-    return 0;
 }
+
