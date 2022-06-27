872bd73f445a14c549431aa7fcf4199c843cc450
eletc <eletckaia2002@gmail.com>
2022-06-27 18:19:30 +0300

    Create README.md

--- tests.cpp
+++ tests.cpp
@@ -1,8 +1,23 @@
 #include "main.cpp"
 #include "doctest.h"
 
-TEST_CASE("testing set_ships_rand")
+TEST_CASE("doctest works")
+{
+    REQUIRE(true);
+}
+
+TEST_CASE("testing map_show")
 {
 
+}
+
+TEST_CASE("testing shot")
+
+{
+    int shot;
+    CHECK (result == 3);
+    CHECK (result == 2);
+    CHECK (result == 1);
+    CHECK (result == -1);
 }
 
