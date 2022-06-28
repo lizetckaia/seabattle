bfb5c059ae2b7ee182fc01e0e416eceefc4e5b78
eletc <eletckaia2002@gmail.com>
2022-06-28 17:30:07 +0300

    Create README.md

--- main.cpp
+++ main.cpp
@@ -189,53 +189,6 @@ void set_ships_rand(int map[N][N],int ship_size, int ship_id)
 
 
 /**
- * функция, которая возвращает целое значение в зависимости от того, ранен корабль, убит, или был совершен промах
- * @param map поле
- * @param x координата х
- * @param y координата у
- * @param ships ссылка на массив, в котором хранится информация о жизнях корабля
- * @param mask ссылка на маску
- * @return
- */
-
-int shot(int map[N][N], int x, int y, int ships[NUM_SHIPS+1], int mask[N][N])
-{
-    ///переменная, которая отвечает за результат стрельбы
-    int result = 0;
-
-    if (map[x][y] == -1 || map[x][y] == -2)
-    {
-        result = 3;
-    }
-
-    else if (map[x][y] >= 1)
-    {
-        ships[map[x][y]]--;
-
-        if (ships[map[x][y]] <= 0)
-        {
-            result = 2;
-        }
-
-        else
-        {
-            result = 1;
-        }
-
-        map[x][y] = -1;
-    }
-
-    else
-    {
-        map[x][y] = -2;
-    }
-
-    mask[x][y] = 1;
-
-    return result;
-}
-
-/**
  * основная функция
  * @return начинает саму игру
  */
--- tests.cpp
+++ tests.cpp
@@ -7,14 +7,41 @@ TEST_CASE("doctest works")
     REQUIRE(true);
 }
 
-TEST_CASE("testing map_show")
-{
-
-}
-
 TEST_CASE("shot")
 
 {
+    ///корабли, которые будут расставляться на поле игрока
+    int ships[NUM_SHIPS+1] = {0, 4,3,3,2,2,2,1,1,1};
 
+    ///карта, принадлежащая человеку
+    int map [N][N] = {0};
+
+    ///переменные, содержащие координаты, по которым стреляет человек
+    int x = 0, y = 0;
+
+    int mask [N][N] = {0};
+
+    int result = 0;
+
+    if (map[x][y] == -1 || map[x][y] == -2)
+    {
+        CHECK (result == 3);
+    }
+
+    else if (map[x][y] >= 1)
+    {
+        ships[map[x][y]]--;
+
+        if (ships[map[x][y]] <= 0)
+        {
+            CHECK (result == 2);
+        }
+
+        else
+        {
+            CHECK (result == 1);
+        }
+
+    }
 
 }
